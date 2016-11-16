8ab5c3907afb2831c74117726cf95accf2f80775
guodp <guodp9u0@gmail.com>
2016-11-16 17:11:53 +0800

    20161116 by guodp for 增加左倾堆Demo

---
+++ AlgorithmsDataStructuresTest/LHeap/LHeap.cs
@@ -0,0 +1,124 @@
+﻿
+using System;
+
+namespace AlgorithmsDataStructuresTest {
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class LHeap {
+
+        public LHeap() {
+
+        }
+
+        private LNode _root;
+
+        public void Print() {
+            PrintInternal(_root, 1);
+        }
+
+        private void PrintInternal(LNode node, int deepth) {
+            if (node == null) {
+                return;
+            }
+            PrintInternal(node.Right, deepth + 1);
+            for (int i = 1; i < deepth; i++) {
+                Console.Write("       ");
+            }
+            Console.WriteLine(string.Format("{0}({1})", node.Element, node.NPL));
+            Console.WriteLine();
+            PrintInternal(node.Left, deepth + 1);
+        }
+
+        public void Insert(int element) {
+            var insertingNode = new LNode(element);
+            _root = MergeInternal(_root, insertingNode);
+        }
+
+        public void Merge(LHeap toMerge) {
+            _root = MergeInternal(_root, toMerge._root);
+        }
+
+        private LNode MergeInternal(LNode node1, LNode node2) {
+            if (node1 == null) {
+                return node2;
+            }
+            if (node2 == null) {
+                return node1;
+            }
+            var newRootNode = node1.Element >= node2.Element ? node2 : node1;
+            var toAppendNode = node1.Element >= node2.Element ? node1 : node2;
+            newRootNode.Right = MergeInternal(newRootNode.Right, toAppendNode);
+            Balance(newRootNode);
+            UpdateNpl(newRootNode);
+            return newRootNode;
+        }
+
+        private void Balance(LNode root) {
+            if (root.Right == null) {
+                return;
+            }
+            if (root.Left != null && root.Left.NPL > root.Right.NPL) {
+                return;
+            }
+
+            var swapTemp = root.Left;
+            root.Left = root.Right;
+            root.Right = swapTemp;
+        }
+
+        private void UpdateNpl(LNode root) {
+            root.NPL = Math.Min(root.Left == null ? 0 : root.Left.NPL,
+                root.Right == null ? 0 : root.Right.NPL)
+                + 1;
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public class LNode {
+
+            /// <summary>
+            /// 
+            /// </summary>
+            /// <param name="element"></param>
+            public LNode(int element) {
+                Element = element;
+            }
+
+            /// <summary>
+            /// 
+            /// </summary>
+            public int Element {
+                get;
+                private set;
+            }
+
+            /// <summary>
+            /// 
+            /// </summary>
+            public LNode Left {
+                get;
+                set;
+            }
+
+            /// <summary>
+            /// 
+            /// </summary>
+            public LNode Right {
+                get;
+                set;
+            }
+
+            /// <summary>
+            /// 
+            /// </summary>
+            public int NPL {
+                get;
+                set;
+            }
+        }
+    }
+}
---
+++ AlgorithmsDataStructuresTest/Program.cs
@@ -0,0 +1,62 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructuresTest {
+    class Program {
+        const int count = 10;
+        static Random random = new Random();
+        static List<int> inputs1 = new List<int>();
+        static List<int> inputs2 = new List<int>();
+        static void Main(string[] args) {
+            var heap1 = new LHeap();
+            var heap2 = new LHeap();
+            while (inputs1.Count < count) {
+                var input = random.Next(10, 99);
+                if (!inputs1.Contains(input)) {
+                    inputs1.Add(input);
+                    heap1.Insert(input);
+                }
+            }
+
+            Console.WriteLine("Inputs1:");
+            foreach (var item in inputs1) {
+                Console.Write(item);
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("LHeap1:");
+            heap1.Print();
+
+            while (inputs2.Count < count) {
+                var input = random.Next(10, 99);
+                if (!inputs1.Contains(input) && !inputs2.Contains(input)) {
+                    inputs2.Add(input);
+                    heap2.Insert(input);
+                }
+            }
+
+            Console.WriteLine("Inputs2:");
+            foreach (var item in inputs2) {
+                Console.Write(item);
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("LHeap2:");
+            heap2.Print();
+
+            LHeap heapResult = new LHeap();
+
+            heapResult.Merge(heap1);
+            Console.WriteLine("After Merge LHeap1 :");
+            heapResult.Print();
+
+            heapResult.Merge(heap2);
+            Console.WriteLine("After Merge LHeap2 :");
+            heapResult.Print();
+        }
+    }
+}
