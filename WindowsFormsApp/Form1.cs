1ed16668cf4a1370062a2f0e4b86fefb1fe7ad6a
guodp <guodp9u0@gmail.com>
2016-12-06 16:55:28 +0800

    20161206 by guodp for WPF Demo

---
+++ WindowsFormsApp/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp {
+    public partial class Form1 : Form {
+
+        delegate string InvokeDelegate();
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            Thread thread = new Thread(() => {
+                if (InvokeRequired) {
+                    Invoke(new InvokeDelegate(() => {
+                        Text = DateTime.Now.Ticks.ToString();
+                        return Text;
+                    }));
+                } else {
+                    Text = "Test:" + DateTime.Now.Ticks.ToString();
+                }
+            });
+            thread.Start();
+        }
+    }
+}
