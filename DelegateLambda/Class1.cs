30b358b135c92b81fec29ad3d03e08d3cad3baef
guodp <guodp9u0@gmail.com>
2016-12-07 11:15:10 +0800

    20161207 by guodp for Lambda

---
+++ DelegateLambda/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateLambda {
+    class Class1 {
+
+        public void Test0() {
+            //Create anonymous object
+            var person = new {
+                Name = "Jesse",
+                Age = 28,
+                /*
+                //错误	CS0828	无法将 lambda 表达式 赋予匿名类型属性	DelegateLambda       
+                Ask = (string question) => {
+                    Console.WriteLine("The answer to `" + question + "` is certainly 42!");
+                }*/
+            };
+
+            //Execute function
+            //person.Ask("Why are you doing this?");
+        }
+        public void Test1() {
+            var person = new {
+                Name = "Florian",
+                Age = 28,
+                Ask = (Action<string>)((string question) => {
+                    Console.WriteLine("The answer to `" + question + "` is certainly 42!");
+                })
+            };
+            //Execute function
+            person.Ask("Why are you doing this?");
+        }
+
+
+
+        public void Test2() {
+
+            dynamic person = null;
+            person = new {
+                Name = "Jesse",
+                Age = 28,
+                Ask = (Action<string>)((string question) => {
+                    Console.WriteLine("The answer to `" + question + "` is certainly 42! My age is " + person.Age + ".");
+                })
+            };
+
+            //Execute function
+            person.Ask("Why are you doing this?");
+        }
+    }
+}
---
+++ DelegateLambda/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateLambda {
+    class Program {
+        static void Main() {
+            Console.WriteLine("-----------------------");
+            new Program().Main1();
+            Console.WriteLine("-----------------------");
+            new Program().Main2();
+            Console.WriteLine("-----------------------");
+            new Program().Main3();
+            Console.WriteLine("-----------------------");
+            new Program().Main4();
+            Console.Read();
+        }
+
+
+
+        void Main1() {
+            Action<string> DoSomethingLambda = (s) => {
+                Console.WriteLine(s);
+            };
+        }
+
+        void Main2() {
+
+            int local = 5;
+
+            Action<string> DoSomethingLambdaClosure = (s) => {
+                Console.WriteLine(s + local);
+            };
+
+            local += 1;
+
+            global = local;
+
+            DoSomethingLambdaClosure("Test 1");
+            DoSomethingNormalClosure("Test 2");
+        }
+
+        void Main3() {
+            int local = 5;
+
+            Action<string> DoSomethingLambdaClosure = (s) => {
+                Console.WriteLine(s + global);
+            };
+
+            local += 1;
+
+            global = local;
+
+            DoSomethingLambdaClosure("Test 1");
+            DoSomethingNormalClosure("Test 2");
+        }
+
+        void Main4() {
+            /*           
+            错误	CS0834	无法将具有语句体的 lambda 表达式转换为表达式树
+             *  */
+            //Expression<Func<MyModel, int>> expr = (model) => { return model.MyProperty;};
+            Expression<Func<MyModel, int>> expr = (model) => model.MyProperty;
+            var member = expr.Body as MemberExpression;
+            var propertyName = member.Member.Name;
+            Console.WriteLine("PropertyName:" + propertyName);
+        }
+
+        int global;
+
+        void DoSomethingNormalClosure(string s) {
+            Console.WriteLine(s + global);
+        }
+
+
+
+        void DoSomethingNormal(string s) {
+            Console.WriteLine(s);
+        }
+
+    }
+    public class MyModel {
+        public int MyProperty {
+            get; set;
+        }
+    }
+}
